1e4addb0410ebd7487349b441770db4923f4bb1d
Sammy-143 <bcict20060@gmail.com>
2024-02-28 03:57:21 -0800

    updaated

--- src/app/grid-container/grid-container.component.ts
+++ src/app/grid-container/grid-container.component.ts
@@ -1,4 +1,3 @@
-// import { Component } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Router } from '@angular/router';
